9bd3ffb42510283aee5847ec8bafcd7d28cd3d34
MackRoe <elaine.music@students.makeschool.com>
2019-09-03 19:20:09 -0700

    finish debugging starter code and begin writing args for is_word_guessed()

--- spaceman.py
+++ spaceman.py
@@ -11,17 +11,30 @@ def load_word():
     words_list = words_list[0].split(' ')
     secret_word = random.choice(words_list)
 return secret_word
-def is_word_guessed(secret_word, letters_guessed):
 
+# function to split string into list of letters_guessed
+def split(secret_word, word_letters):
+    word_letters = list(secret_word)
+    return word_letters
+
+
+def is_word_guessed(secret_word, letters_guessed):
+    # split the string into a list of letters
+    word_letters() = list(secret_word)
+    print(word_letters())
     # A function that checks if all the letters of the secret word have been guessed.
     # Args:
         # secret_word (string): the random word the user is trying to guess.
         # letters_guessed (list of strings): list of letters that have been guessed so far.
     # Returns:
+    if word_letters == letters_guessed:
+        return True
+    else:
+        return False
         # bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
-    
+
 # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
-pass
+# pass (commented out unfamiliar command)
 def get_guessed_word(secret_word, letters_guessed):
 
     # A function that is used to get a string showing the letters guessed so far in the secret word and underscores for letters that have not been guessed yet.
@@ -35,7 +48,7 @@ def get_guessed_word(secret_word, letters_guessed):
 #TODO: Loop through the letters in secret word and build a string that shows the letters that have been
     # guessed correctly so far that are saved in letters_guessed and underscores for the letters that have
     # not been guessed yet
-pass
+    pass
 def is_guess_in_word(guess, secret_word):
 
     # A function to check if the guessed letter is in the secret word
@@ -46,7 +59,7 @@ def is_guess_in_word(guess, secret_word):
         # bool: True if the guess is in the secret_word, False otherwise
 
 #TODO: check if the letter guess is in the secret word
-pass
+    pass
 def spaceman(secret_word):
 
     # A function that controls the game of spaceman. Will start spaceman in the command line.
@@ -59,5 +72,10 @@ def spaceman(secret_word):
 #TODO: show the guessed word so far
 #TODO: check if the game has been won or lost
 #These function calls that will start the game
+    pass
 secret_word = load_word()
 spaceman(load_word())
+# test
+load_word()
+print(secret_word)
+# end test
